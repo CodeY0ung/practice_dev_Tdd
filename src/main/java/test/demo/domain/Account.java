5ccb0e28b9b6463e437da74d3a4ba7709005b0dd
CodeY0ung <wooyounglee3312@naver.com>
2026-09-03 11:29:52 +0900

    account Entity화

--- src/main/java/test/demo/domain/Account.java
+++ src/main/java/test/demo/domain/Account.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.NoArgsConstructor;
 import test.demo.exception.InsufficientBalanceException;
 import test.demo.exception.InvalidAmountException;
 
@@ -14,40 +13,47 @@ public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private int balance;
 
-    protected Account(){
-
+    protected Account() {
     }
 
-    public Account(Long id, int balance) {
-        this.id = id;
+    public Account(int balance) {
         this.balance = balance;
     }
 
-    public Long getId(){
+    public Long getId() {
         return id;
     }
-    public int getBalance(){
+
+    public int getBalance() {
         return balance;
     }
 
-    // 입금
-    public void deposit(int amount){
-        if(amount <= 0){
-            throw new IllegalArgumentException("입금 금액은 0보다 커야 합니다.");
+    public void deposit(int amount) {
+        if (amount <= 0) {
+            throw new InvalidAmountException(
+                    "입금 금액은 0보다 커야 합니다."
+            );
         }
+
         balance += amount;
     }
 
-    // 출금
-    public void withdraw(int amount){
-        if(amount > balance){
-            throw new InsufficientBalanceException("잔액이 부족합니다.");
+    public void withdraw(int amount) {
+        if (amount <= 0) {
+            throw new InvalidAmountException(
+                    "출금 금액은 0보다 커야 합니다."
+            );
         }
-        else if(amount <= 0){
-            throw new InvalidAmountException("출금 금액은 0보다 커야합니다.");
+
+        if (amount > balance) {
+            throw new InsufficientBalanceException(
+                    "잔액이 부족합니다."
+            );
         }
+
         balance -= amount;
     }
 }
